b9460cd50b373e845f2ff5385e03bfaa5e3463f1
Benedict Adamson <badamson@spamcop.net>
2019-04-20 12:29:01 +0100

    Generate FE player component.

--- MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent
+    PlayersComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
