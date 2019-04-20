79689ed052f5c45785a300309ce0a29759c806a8
Benedict Adamson <badamson@spamcop.net>
2019-04-20 18:34:58 +0100

    Add test of FE PlayerComponent ctor.

--- MC-front-end/src/main/web/mc-front-end/src/app/player/player.component.spec.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/player/player.component.spec.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { Observable, of } from 'rxjs';
 
+import { Player } from '../player';
 import { PlayerComponent } from './player.component';
 import { PlayerService } from '../player.service';
 
@@ -10,32 +11,38 @@ describe('PlayerComponent', () => {
     let component: PlayerComponent;
     let fixture: ComponentFixture<PlayerComponent>;
 
-    beforeEach(async(() => {
+    const PLAYER_A = { username: 'Administrator', password: null, authorities: ['ROLE_ADMIN'] };
+    const PLAYER_B = { username: 'Benedict', password: null, authorities: [] };
+
+    const canCreate = (testPlayer: Player) => {
         const playerServiceStub = jasmine.createSpyObj('PlayerService', ['getPlayer']);
-        playerServiceStub.getPlayer.and.returnValue( of({ username: 'Administrator' }) );
+        playerServiceStub.getPlayer.and.returnValue(of(testPlayer));
 
         TestBed.configureTestingModule({
             declarations: [PlayerComponent],
             providers: [{
                 provide: ActivatedRoute,
                 useValue: {
-                    params: of({ username: 'Administrator' }),
+                    params: of({ username: testPlayer.username }),
                     snapshot: {
-                        paramMap: convertToParamMap({ username: 'Administrator' })
+                        paramMap: convertToParamMap({ username: testPlayer.username })
                     }
                 }
             },
             { provide: PlayerService, useValue: playerServiceStub }]
         });
-    }));
 
-    beforeEach(() => {
         fixture = TestBed.createComponent(PlayerComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-    });
 
-    it('should create', () => {
         expect(component).toBeTruthy();
+        expect(component.player).toBe(testPlayer);
+    };
+    it('can create [a]', () => {
+        canCreate(PLAYER_A);
+    });
+    it('can create [b]', () => {
+        canCreate(PLAYER_B);
     });
 });
