f68d2e3345095dc12350804235af6306444588ee
Benedict Adamson <badamson@spamcop.net>
2019-04-20 13:09:48 +0100

    Create a basic home page component.

--- MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { PlayerComponent } from './player/player.component';
 import { PlayersComponent } from './players/players.component';
 
 const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'player', component: PlayersComponent, pathMatch: 'full' },
     { path: 'player/:username', component: PlayerComponent }
 ];
