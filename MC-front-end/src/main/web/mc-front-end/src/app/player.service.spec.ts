66d3165641ac32136a0aa8013f9c38a66b706f3d
Benedict Adamson <badamson@spamcop.net>
2019-04-20 17:14:10 +0100

    FE PlayerService uses HTTP

--- MC-front-end/src/main/web/mc-front-end/src/app/player.service.spec.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/player.service.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { PlayerService } from './player.service';
+import { Player } from './player';
 
 describe('PlayerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
 
-  it('should be created', () => {
-    const service: PlayerService = TestBed.get(PlayerService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+
+        /* Inject for each test:
+         * HTTP requests will be handled by the mock back-end.
+          */
+        httpClient = TestBed.get(HttpClient);
+        httpTestingController = TestBed.get(HttpTestingController);
+    });
+
+    it('should be created', () => {
+        const service: PlayerService = TestBed.get(PlayerService);
+        expect(service).toBeTruthy();
+    });
+
+    it('can get players', () => {
+        const testPlayers: Player[] = [{ username: 'Administrator', password: null, authorities: ['ROLE_ADMIN'] }];
+        const service: PlayerService = TestBed.get(PlayerService);
+
+        service.getPlayers().subscribe(players => expect(players).toEqual(testPlayers));
+
+        const request = httpTestingController.expectOne('/api/player');
+        expect(request.request.method).toEqual('GET');
+        request.flush(testPlayers);
+        httpTestingController.verify();
+    });
 });
--- MC-front-end/src/main/web/mc-front-end/src/app/player.service.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/player.service.ts
@@ -1,21 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 import { Player } from './player';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
     providedIn: 'root'
 })
 export class PlayerService {
 
-    private players: Player[] = [{ username: 'Administrator', password: null, authorities: ['ROLE_ADMIN'] }];
+    private playerUrl = '/api/player';  // URL to API
 
-    constructor() { }
+    constructor(
+        private http: HttpClient) { }
 
     getPlayers(): Observable<Player[]> {
-        return of(this.players);
+        return this.http.get<Player[]>(this.playerUrl)
+            .pipe(
+                catchError(this.handleError<Player[]>('getPlayers', []))
+            );
     }
 
     getPlayer(username: string): Observable<Player> {
-        return of(this.players.find(player => player.username === username));
+        const url = `${this.playerUrl}/${username}`;
+        return this.http.get<Player>(url)
+            .pipe(
+                catchError(this.handleError<Player>(`getPlayer id=${username}`))
+            );
+    }
+
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
     }
 }
