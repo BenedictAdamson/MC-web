a1e67ab02d38f7d276514da43e9c774e53ce5e88
Benedict Adamson <badamson@spamcop.net>
2019-04-20 11:11:14 +0100

    Add routing for /player

--- MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayersComponent } from './players/players.component';
+
+const routes: Routes = [
+    { path: 'player', component: PlayersComponent }
+];
 
 @NgModule({
+    imports: [RouterModule.forRoot(routes)],
     exports: [
         RouterModule
     ]
