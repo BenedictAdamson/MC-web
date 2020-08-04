31c634822a2e3aa62dcb0c9fb2db2b9a5a8c444f
Benedict Adamson <badamson@spamcop.net>
2020-08-04 13:41:46 +0100

    Extract duplicate test code

--- MC-integration/src/test/java/uk/badamson/mc/McBackEndContainer.java
+++ MC-integration/src/test/java/uk/badamson/mc/McBackEndContainer.java
@@ -18,11 +18,6 @@ package uk.badamson.mc;
  * along with MC.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.testcontainers.containers.GenericContainer;
 
 /**
@@ -32,37 +27,10 @@ import org.testcontainers.containers.GenericContainer;
  */
 final class McBackEndContainer extends GenericContainer<McBackEndContainer> {
 
-   public static final String VERSION = getVersion();
+   public static final String VERSION = Version.VERSION;
 
    public static final String IMAGE = "index.docker.io/benedictadamson/mc-back-end:"
             + VERSION;
-
-   private static Properties getApplicationProperties() throws IOException {
-      final InputStream stream = Thread.currentThread().getContextClassLoader()
-               .getResourceAsStream("application.properties");
-      if (stream == null) {
-         throw new FileNotFoundException(
-                  "resource application.properties not found");
-      }
-      final Properties properties = new Properties();
-      properties.load(stream);
-      return properties;
-   }
-
-   private static String getVersion() {
-      String version;
-      try {
-         version = getApplicationProperties().getProperty("build.version");
-      } catch (final IOException e) {
-         throw new IllegalStateException(
-                  "unable to read application.properties resource", e);
-      }
-      if (version == null || version.isEmpty()) {
-         throw new IllegalStateException(
-                  "missing build.version property in application.properties resource");
-      }
-      return version;
-   }
 
    McBackEndContainer() {
       super(IMAGE);
