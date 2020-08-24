89eb0a7ab9b5751fab1ab546115c838d6511ddd6
Benedict Adamson <badamson@spamcop.net>
2020-08-24 14:53:08 +0100

    Add Keycloak authentication guards code to FE

--- MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
@@ -1,19 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth-guard';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
 
+/*
+ * AuthGuard enforces that the user must be authenticated and have all the roles in the data.roles array,
+ * if data.roles is not empty.
+ */
 const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'user', component: UsersComponent, pathMatch: 'full' },
-    { path: 'user/:username', component: UserComponent }
+	{
+		path: '',
+		component: HomeComponent,
+		pathMatch: 'full',
+		canActivate: [AuthGuard],
+		data: { roles: [] }
+	},
+	{
+		path: 'user',
+		component: UsersComponent,
+		pathMatch: 'full',
+		data: { roles: [] }
+	},
+	{
+		path: 'user/:username',
+		component: UserComponent,
+		data: { roles: [] }
+	}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [
-        RouterModule
-    ]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule { }
