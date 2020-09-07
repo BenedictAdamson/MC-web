12f65f0475f0d38d35886cb5add80dea97c89aeb
Benedict Adamson <badamson@spamcop.net>
2020-09-07 23:09:37 +0100

    Better diagnostics for failure to set up test
    In McAuthContainer.addPlayer(String, String)

--- MC-integration/src/test/java/uk/badamson/mc/auth/McAuthContainer.java
+++ MC-integration/src/test/java/uk/badamson/mc/auth/McAuthContainer.java
@@ -28,6 +28,7 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.containers.wait.strategy.WaitStrategy;
+import org.testcontainers.containers.Container;
 
 import uk.badamson.mc.Version;
 
@@ -50,6 +51,7 @@ public final class McAuthContainer extends GenericContainer<McAuthContainer> {
    public static final String DB_NAME = "keycloak";
    public static final String MC_REALM = "MC";
    public static final String MC_CLIENT_ID = "mc-ui";
+   public static final String REALM_MANAGEMENT_CLIENT_ID = "realm-management";
 
    private static final Duration STARTUP_TIME = Duration.ofSeconds(180);
 
@@ -69,22 +71,30 @@ public final class McAuthContainer extends GenericContainer<McAuthContainer> {
       waitingFor(WAIT_STRATEGY);
    }
 
+   private void requireSuccess(Container.ExecResult result) {
+      if (result.getExitCode() != 0) {
+         final String message = result.getStderr() + "\n" + result.getStdout();
+         throw new IllegalStateException(message);
+      }
+   }
+
    public void addPlayer(String user, String password) {
       try {
-         execInContainer(KCADM, "create", "users", "-r", MC_REALM, "-s",
-                  "username=" + user, "-s", "enabled=true");
-         execInContainer(KCADM, "set-password", "-r", MC_REALM, "--username",
-                  user, "--new-password", password);
-         execInContainer(KCADM, "add-roles", "-r", MC_REALM, "--uusername",
-                  user, "--rolename", "player");
+         requireSuccess(execInContainer(KCADM, "create", "users", "-r",
+                  MC_REALM, "-s", "username=" + user, "-s", "enabled=true"));
+         requireSuccess(execInContainer(KCADM, "set-password", "-r", MC_REALM,
+                  "--username", user, "--new-password", password));
+         requireSuccess(execInContainer(KCADM, "add-roles", "-r", MC_REALM,
+                  "--uusername", user, "--rolename", "player"));
       } catch (IOException | InterruptedException e) {
          throw new RuntimeException("Failed to add player", e);
       }
    }
 
-   public Keycloak getKeycloakInstance(String user, String password) {
+   public Keycloak getKeycloakInstance(String user, String password,
+            String client) {
       return Keycloak.getInstance(getUri().toASCIIString(), MC_REALM, user,
-               password, MC_CLIENT_ID);
+               password, client);
    }
 
    private URI getUri() {
