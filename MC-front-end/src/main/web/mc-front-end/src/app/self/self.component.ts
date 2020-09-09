c6cc6ffc1efc4abf9556edff7922cda05fcc8cc5
Benedict Adamson <badamson@spamcop.net>
2020-09-09 11:31:30 +0100

    Implement FE self component.

--- MC-front-end/src/main/web/mc-front-end/src/app/self/self.component.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/self/self.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
-  selector: 'app-self',
-  templateUrl: './self.component.html',
-  styleUrls: ['./self.component.css']
+	selector: 'app-self',
+	templateUrl: './self.component.html',
+	styleUrls: ['./self.component.css']
 })
 export class SelfComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private readonly router: Router,
+		private readonly keycloak: KeycloakService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	getUsername(): string { return this.keycloak.getUsername(); }
+
+	isLoggedIn(): Promise<boolean> { return this.keycloak.isLoggedIn(); }
+
+	async login() {
+		await this.keycloak.login({
+			redirectUri: window.location.origin + this.router.url
+		});
+	}
 }
