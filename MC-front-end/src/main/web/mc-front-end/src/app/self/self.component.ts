d24f179c048f6bae5ab6981d703c72e8a6c8e03c
Benedict Adamson <badamson@spamcop.net>
2020-09-10 12:15:54 +0100

    FE SelfComponent subscribes to Keycloak events.
    So can update its values.

--- MC-front-end/src/main/web/mc-front-end/src/app/self/self.component.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/self/self.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, defer, from } from 'rxjs';
-import { KeycloakService } from 'keycloak-angular';
+import { Observable, Subject, defer, from } from 'rxjs';
+import { KeycloakService, KeycloakEvent, KeycloakEventType } from 'keycloak-angular';
 
 @Component({
 	selector: 'app-self',
@@ -12,18 +12,43 @@ export class SelfComponent implements OnInit {
 	constructor(
 		private readonly keycloak: KeycloakService) { }
 
-	ngOnInit(): void {
+	async ngOnInit(): Promise<void> {
+		this.keycloak.keycloakEvents$.subscribe(event => this.handle(event));
+		if (await this.keycloak.isLoggedIn()) {
+			this.handleLoggedIn();
+		} else {
+			this.handleLoggedOut();
+		}
+		/* As the handlers are idempotent, double handling because of subscription and the isLoggedIn() call is OK */
 	}
 
-	getUsername(): string { return this.keycloak.getUsername(); }
+	handleLoggedOut(): void {// idempotent
+		this.username.next(null);
+		this.loggedIn.next(false);
+	}
 
-	/**
-	 * This indirectly makes use of an HTTP request, which is a cold Observable,
-     * so this is a cold Observable too.
-     * That is, the expensive HTTP request will not be made until something subscribes to this Observable.
-	 */
-	loggedIn: Observable<boolean> = defer(() =>
-		from(this.keycloak.isLoggedIn()));
+	handleLoggedIn(): void {// idempotent
+		this.username.next(this.keycloak.getUsername());
+		this.loggedIn.next(true);
+	}
+
+	private handle(event: KeycloakEvent): void {
+		switch (event.type) {
+			case KeycloakEventType.OnAuthError:
+			case KeycloakEventType.OnAuthLogout:
+			case KeycloakEventType.OnAuthRefreshError:
+				this.handleLoggedOut(); break;
+			case KeycloakEventType.OnAuthRefreshSuccess:
+			case KeycloakEventType.OnAuthSuccess:
+				this.handleLoggedIn(); break;
+			default:
+			// ignore
+		}
+	}
+
+	username: Subject<string> = new Subject;
+
+	loggedIn: Subject<boolean> = new Subject;
 
 	/**
 	 * This indirectly makes use of an HTTP request, which is a cold Observable,
