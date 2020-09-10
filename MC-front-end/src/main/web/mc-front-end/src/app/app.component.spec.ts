ec595eb74326991221d353e40ee3b35b256ab4b7
Benedict Adamson <badamson@spamcop.net>
2020-09-10 17:20:33 +0100

    FE always displays SelfComponent

--- MC-front-end/src/main/web/mc-front-end/src/app/app.component.spec.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.component.spec.ts
@@ -1,34 +1,63 @@
-import { TestBed, async } from '@angular/core/testing';
+import { KeycloakService, KeycloakEvent, KeycloakEventType } from 'keycloak-angular';
+import { Subject } from 'rxjs';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { SelfComponent } from './self/self.component';
+
+class MockKeycloakService {
+
+	keycloakEvents$: Subject<KeycloakEvent> = new Subject;
+
+	private username: string = null;
+	private nextUsername: string = "jeff";
+
+	getUsername(): string { return this.username; }
+
+	async isLoggedIn(): Promise<boolean> { return Promise.resolve(this.username != null); }
+
+	async login(options: any): Promise<void> {
+		return new Promise((resolve, reject) => {
+			this.username = this.nextUsername;
+			this.nextUsername = null;
+			this.keycloakEvents$.next({
+				type: KeycloakEventType.OnAuthSuccess
+			});
+			resolve();
+		});
+	}
+}
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                AppComponent
-            ],
-            imports: [RouterTestingModule]
-        }).compileComponents();
-    }));
+	beforeEach(waitForAsync(() => {
+		TestBed.configureTestingModule({
+			providers: [
+				{ provide: KeycloakService, useClass: MockKeycloakService }
+			],
+			declarations: [
+				AppComponent, SelfComponent
+			],
+			imports: [RouterTestingModule]
+		}).compileComponents();
+	}));
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
-    });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app).toBeTruthy();
+	});
 
-    it(`should have as title 'Mission Command'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('Mission Command');
-    });
+	it(`should have as title 'Mission Command'`, () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.debugElement.componentInstance;
+		expect(app.title).toEqual('Mission Command');
+	});
 
-    it('should render title in a h1 tag', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Mission Command');
-    });
+	it('should render title in a h1 tag', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('h1').textContent).toContain('Mission Command');
+	});
 });
