864e85c753643494dc5c37369c2ce0ad5dd04bf7
Benedict Adamson <badamson@spamcop.net>
2020-09-16 21:38:33 +0100

    Remove User, UserService & UserComponent from FE
    Will use Keycloak instead.

--- MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth-guard';
 import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { UsersComponent } from './users/users.component';
 
 /*
  * AuthGuard enforces that the user must be authenticated and have all the roles in the data.roles array,
@@ -15,17 +13,6 @@ const routes: Routes = [
 		component: HomeComponent,
 		pathMatch: 'full',
 		canActivate: [AuthGuard],
-		data: { roles: [] }
-	},
-	{
-		path: 'user',
-		component: UsersComponent,
-		pathMatch: 'full',
-		data: { roles: [] }
-	},
-	{
-		path: 'user/:username',
-		component: UserComponent,
 		data: { roles: [] }
 	}
 ];
