f9f155d3087e5ddbec07e2c4808130169756c40f
Benedict Adamson <badamson@spamcop.net>
2020-09-16 21:37:44 +0100

    Remove User, UserService & UserComponent from FE
    Will use Keycloak instead.

--- MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WINDOW_PROVIDERS } from './window-providers';
 import { KEYCLOAK_PROVIDERS } from './keycloak-init';
-import { UserComponent } from './user/user.component';
-import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { SelfComponent } from './self/self.component';
 
@@ -23,8 +21,6 @@ import { SelfComponent } from './self/self.component';
 	],
 	declarations: [
 		AppComponent,
-		UsersComponent,
-		UserComponent,
 		HomeComponent,
 		SelfComponent
 	],
