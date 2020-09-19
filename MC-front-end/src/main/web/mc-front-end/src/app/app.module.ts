86e78fa6d27f62c088ddbe35782dd077416226f3
Benedict Adamson <badamson@spamcop.net>
2020-09-19 18:06:08 +0100

    Rename FE KEYCLOAK_PROVIDER => KEYCLOAK_INITIALISER
    Because does not actually provide

--- MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WINDOW_PROVIDER } from './window.provider';
-import { KEYCLOAK_INITIALIZER } from './keycloak.provider';
+import { KEYCLOAK_INITIALIZER } from './keycloak.initializer';
 import { HomeComponent } from './home/home.component';
 import { SelfComponent } from './self/self.component';
 
