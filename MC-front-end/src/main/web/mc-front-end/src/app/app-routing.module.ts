57010ef119919f1b36c082b16f613073c12b8e64
Benedict Adamson <badamson@spamcop.net>
2020-09-25 09:08:42 +0100

    Provide a link to login page rather than a login button.

--- MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
 
@@ -12,6 +14,11 @@ const routes: Routes = [
 	{
 		path: '',
 		component: HomeComponent,
+		pathMatch: 'full'
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
 		pathMatch: 'full'
 	},
 	{
