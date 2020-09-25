7d4390d5f47a0a2ab566dd46608d455b75ede98f
Benedict Adamson <badamson@spamcop.net>
2020-09-25 09:30:14 +0100

    FE login form should have a username field

--- MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -17,6 +18,7 @@ import { SelfComponent } from './self/self.component';
 	imports: [
 		BrowserModule,
 		CommonModule,
+		FormsModule,
 		AppRoutingModule,
 		HttpClientModule,
 		RouterModule
