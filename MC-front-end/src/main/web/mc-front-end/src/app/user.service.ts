22924dc43bf6bbb60e5c32ca4a0840f11e315921
Benedict Adamson <badamson@spamcop.net>
2020-10-13 10:52:39 +0100

    Add FE UserService.add()

--- MC-front-end/src/main/web/mc-front-end/src/app/user.service.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/user.service.ts
@@ -1,38 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { User } from './user';
 
 const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserService {
 
-    private userUrl = '/api/user';  // URL to API
+	private userUrl = '/api/user';  // URL to API
 
-    constructor(
-        private http: HttpClient) { }
+	constructor(
+		private http: HttpClient) { }
 
-    getUsers(): Observable<User[]> {
-        return this.http.get<User[]>(this.userUrl)
-            .pipe(
-                catchError(this.handleError<User[]>('getUsers', []))
-            );
-    }
+	getUsers(): Observable<User[]> {
+		return this.http.get<User[]>(this.userUrl)
+			.pipe(
+				catchError(this.handleError<User[]>('getUsers', []))
+			);
+	}
 
-    getUser(username: string): Observable<User> {
-        const url = `${this.userUrl}/${username}`;
-        return this.http.get<User>(url)
-            .pipe(
-                catchError(this.handleError<User>(`getUser id=${username}`))
-            );
-    }
+	getUser(username: string): Observable<User> {
+		const url = `${this.userUrl}/${username}`;
+		return this.http.get<User>(url)
+			.pipe(
+				catchError(this.handleError<User>(`getUser id=${username}`))
+			);
+	}
+
+	/**@description
+	 * Add a given user as a new user.
+     *
+     * @returns
+     * An observable that provides a value indicating whether addition was successful.
+	 */
+	add(user: User): Observable<boolean> {
+		return this.http.post(this.userUrl, user)
+			.pipe(
+				catchError(this.handleError<boolean>(`add id=${user.username}`, false)),
+				map(() => true)
+			);
+	}
 
     /**
      * Handle Http operation that failed.
@@ -40,14 +54,14 @@ export class UserService {
      * @param operation - name of the operation that failed
      * @param result - optional value to return as the observable result
      */
-    private handleError<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
+	private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
 
-            // TODO: send the error to remote logging infrastructure
-            console.error(error); // log to console instead
+			// TODO: send the error to remote logging infrastructure
+			console.error(error); // log to console instead
 
-            // Let the app keep running by returning an empty result.
-            return of(result as T);
-        };
-    }
+			// Let the app keep running by returning an empty result.
+			return of(result as T);
+		};
+	}
 }
