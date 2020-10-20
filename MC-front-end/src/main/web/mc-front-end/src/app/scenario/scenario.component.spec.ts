f71e658758f561a1e54a48270fa5a9ef5ccb330e
Benedict Adamson <badamson@spamcop.net>
2020-10-20 09:33:49 +0100

    Add test that FE serves correct description of scenarios

--- MC-front-end/src/main/web/mc-front-end/src/app/scenario/scenario.component.spec.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/scenario/scenario.component.spec.ts
@@ -9,46 +9,48 @@ import { ScenarioService } from '../scenario.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('ScenarioComponent', () => {
-    let component: ScenarioComponent;
-    let fixture: ComponentFixture<ScenarioComponent>;
+	let component: ScenarioComponent;
+	let fixture: ComponentFixture<ScenarioComponent>;
 
-	const IDENTIFIER_A: ScenarioIdentifier = { id: '123456', title: 'Section Attack'};
-	const IDENTIFIER_B: ScenarioIdentifier = { id: '345678', title: 'Beach Assault'};
+	const IDENTIFIER_A: ScenarioIdentifier = { id: '123456', title: 'Section Attack' };
+	const IDENTIFIER_B: ScenarioIdentifier = { id: '345678', title: 'Beach Assault' };
 	const SCENARIO_A: Scenario = { identifier: IDENTIFIER_A, description: 'Basic fire-and-movement tactical training.' };
 	const SCENARIO_B: Scenario = { identifier: IDENTIFIER_B, description: 'Fast and deadly.' };
 
-    const setUp = (testScenario: Scenario) => {
-        const scenarioServiceStub = jasmine.createSpyObj('ScenarioService', ['getScenario']);
-        scenarioServiceStub.getScenario.and.returnValue(of(testScenario));
+	const setUp = (testScenario: Scenario) => {
+		const scenarioServiceStub = jasmine.createSpyObj('ScenarioService', ['getScenario']);
+		scenarioServiceStub.getScenario.and.returnValue(of(testScenario));
 
-        TestBed.configureTestingModule({
-            declarations: [ScenarioComponent],
-            providers: [{
-                provide: ActivatedRoute,
-                useValue: {
-                    params: of({ id: testScenario.identifier.id }),
-                    snapshot: {
-                        paramMap: convertToParamMap({ id: testScenario.identifier.id })
-                    }
-                }
-            },
-            { provide: ScenarioService, useValue: scenarioServiceStub }]
-        });
+		TestBed.configureTestingModule({
+			declarations: [ScenarioComponent],
+			providers: [{
+				provide: ActivatedRoute,
+				useValue: {
+					params: of({ id: testScenario.identifier.id }),
+					snapshot: {
+						paramMap: convertToParamMap({ id: testScenario.identifier.id })
+					}
+				}
+			},
+			{ provide: ScenarioService, useValue: scenarioServiceStub }]
+		});
 
-        fixture = TestBed.createComponent(ScenarioComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    };
-    const canCreate = (testScenario: Scenario) => {
-        setUp(testScenario);
+		fixture = TestBed.createComponent(ScenarioComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	};
+	const canCreate = (testScenario: Scenario) => {
+		setUp(testScenario);
 
-        expect(component).toBeTruthy();
-        expect(component.scenario).toBe(testScenario);
-    };
-    it('can create [a]', () => {
-        canCreate(SCENARIO_A);
-    });
-    it('can create [b]', () => {
-        canCreate(SCENARIO_B);
-    });
+		expect(component).toBeTruthy();
+		expect(component.scenario).toBe(testScenario);
+		const displayText: string = fixture.nativeElement.innerText;
+		expect(displayText.includes(testScenario.description)).withContext("displayed text includes description").toBeTrue();
+	};
+	it('can create [a]', () => {
+		canCreate(SCENARIO_A);
+	});
+	it('can create [b]', () => {
+		canCreate(SCENARIO_B);
+	});
 });
