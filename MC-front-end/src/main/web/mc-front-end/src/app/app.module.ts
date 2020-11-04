168882163cdc6734d414328e0af8ca7803fffa71
Benedict Adamson <badamson@spamcop.net>
2020-11-04 11:53:47 +0000

    Add (dummy) GamComponent to FE

--- MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
+import { WINDOW_PROVIDER } from './window.provider';
+
 import { AddUserComponent } from './add-user/add-user.component';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { GameComponent } from './game/game.component';
 import { LoginComponent } from './login/login.component';
 import { ScenarioComponent } from './scenario/scenario.component';
 import { ScenariosComponent } from './scenarios/scenarios.component';
 import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
-import { WINDOW_PROVIDER } from './window.provider';
 import { HomeComponent } from './home/home.component';
 import { SelfComponent } from './self/self.component';
 
@@ -35,7 +37,8 @@ import { SelfComponent } from './self/self.component';
 		ScenariosComponent,
 		SelfComponent,
 		UsersComponent,
-		UserComponent
+		UserComponent,
+		GameComponent
 	],
 	providers: [
 		WINDOW_PROVIDER
