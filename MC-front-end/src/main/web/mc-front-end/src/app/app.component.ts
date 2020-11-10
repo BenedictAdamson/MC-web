a8c488af75d22fac7e14f78855212206e493ec6d
Benedict Adamson <badamson@spamcop.net>
2020-11-10 10:25:18 +0000

    FE AppComponent ASSOCIATES-WITH-A SelfService.

--- MC-front-end/src/main/web/mc-front-end/src/app/app.component.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { SelfService } from './self.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	
+	constructor(
+		private readonly selfService: SelfService
+		){}
 }
