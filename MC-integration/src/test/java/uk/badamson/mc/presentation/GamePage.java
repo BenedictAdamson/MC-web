d6e4c96bcae706741e4ce70aaacdfefb8a8b110b
Benedict Adamson <badamson@spamcop.net>
2020-11-18 12:04:35 +0000

    Fix typo in IT

--- MC-integration/src/test/java/uk/badamson/mc/presentation/GamePage.java
+++ MC-integration/src/test/java/uk/badamson/mc/presentation/GamePage.java
@@ -60,7 +60,7 @@ public final class GamePage extends Page {
    private static final By SCENARIO_LINK_LOCATOR = By.id("scenario");
    private static final By RECRUITING_ELEMENT_LOCATOR = By.id("recruiting");
    private static final By END_RECRUITMENT_BUTTON_LOCATOR = By
-            .id("end-recuiting");
+            .id("end-recruiting");
 
    private static boolean isEnabled(final WebElement button) {
       return button.getAttribute("disabled") != null;
