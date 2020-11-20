de4693addb1ded4309d36e6e418c2746f240af06
Benedict Adamson <badamson@spamcop.net>
2020-11-20 09:53:34 +0000

    FE user page includes the user name

--- MC-front-end/src/main/web/mc-front-end/src/app/user/user.component.spec.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/user/user.component.spec.ts
@@ -8,44 +8,48 @@ import { UserService } from '../user.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('UserComponent', () => {
-    let component: UserComponent;
-    let fixture: ComponentFixture<UserComponent>;
+	let component: UserComponent;
+	let fixture: ComponentFixture<UserComponent>;
 
-    const USER_A = { username: 'Administrator', password: null, authorities: ['ROLE_ADMIN'] };
-    const USER_B = { username: 'Benedict', password: null, authorities: [] };
+	const USER_A = { username: 'Administrator', password: null, authorities: ['ROLE_ADMIN'] };
+	const USER_B = { username: 'Benedict', password: null, authorities: [] };
 
-    const setUp = (testUser: User) => {
-        const userServiceStub = jasmine.createSpyObj('UserService', ['getUser']);
-        userServiceStub.getUser.and.returnValue(of(testUser));
+	const setUp = (testUser: User) => {
+		const userServiceStub = jasmine.createSpyObj('UserService', ['getUser']);
+		userServiceStub.getUser.and.returnValue(of(testUser));
 
-        TestBed.configureTestingModule({
-            declarations: [UserComponent],
-            providers: [{
-                provide: ActivatedRoute,
-                useValue: {
-                    params: of({ username: testUser.username }),
-                    snapshot: {
-                        paramMap: convertToParamMap({ username: testUser.username })
-                    }
-                }
-            },
-            { provide: UserService, useValue: userServiceStub }]
-        });
+		TestBed.configureTestingModule({
+			declarations: [UserComponent],
+			providers: [{
+				provide: ActivatedRoute,
+				useValue: {
+					params: of({ username: testUser.username }),
+					snapshot: {
+						paramMap: convertToParamMap({ username: testUser.username })
+					}
+				}
+			},
+			{ provide: UserService, useValue: userServiceStub }]
+		});
 
-        fixture = TestBed.createComponent(UserComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    };
-    const canCreate = (testUser: User) => {
-        setUp(testUser);
+		fixture = TestBed.createComponent(UserComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	};
+	const canCreate = (testUser: User) => {
+		setUp(testUser);
 
-        expect(component).toBeTruthy();
-        expect(component.user).toBe(testUser);
-    };
-    it('can create [a]', () => {
-        canCreate(USER_A);
-    });
-    it('can create [b]', () => {
-        canCreate(USER_B);
-    });
+		expect(component).toBeTruthy();
+		expect(component.user).toBe(testUser);
+
+		const html: HTMLElement = fixture.nativeElement;
+		const displayText: string = html.innerText;
+		expect(displayText.includes(testUser.username)).withContext("The user page includes the user name").toBeTrue();
+	};
+	it('can create [a]', () => {
+		canCreate(USER_A);
+	});
+	it('can create [b]', () => {
+		canCreate(USER_B);
+	});
 });
