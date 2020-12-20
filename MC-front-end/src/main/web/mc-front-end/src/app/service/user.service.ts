a5b3922a8a3c4d0c09cfb52eeb42a0a7cfd6ead4
Benedict Adamson <badamson@spamcop.net>
2020-12-20 22:06:15 +0000

    Extract method in FE.

--- MC-front-end/src/main/web/mc-front-end/src/app/service/user.service.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/service/user.service.ts
@@ -12,21 +12,24 @@ import { UserDetails } from '../user-details';
 })
 export class UserService {
 
-	private userUrl = '/api/user';  // URL to API
+	private static apiUsersPath: string = '/api/user';
+
+	static getApiUserPath(id: string): string {
+		return UserService.apiUsersPath + '/' + id;
+	}
 
 	constructor(
 		private http: HttpClient) { }
 
 	getUsers(): Observable<User[]> {
-		return this.http.get<User[]>(this.userUrl)
+		return this.http.get<User[]>(UserService.apiUsersPath)
 			.pipe(
 				catchError(this.handleError<User[]>('getUsers', []))
 			);
 	}
 
 	getUser(id: string): Observable<User | null> {
-		const url = `${this.userUrl}/${id}`;
-		return this.http.get<User>(url)
+		return this.http.get<User>(UserService.getApiUserPath(id))
 			.pipe(
 				catchError(this.handleError<User>(`getUser id=${id}`))
 			);
@@ -34,26 +37,26 @@ export class UserService {
 
 	/**@description
 	 * Add a user with given details as a new user.
-     *
-     * @returns
-     * An observable that provides the added user, or null or there is an error.
+	 *
+	 * @returns
+	 * An observable that provides the added user, or null or there is an error.
 	 */
 	add(userDetails: UserDetails): Observable<User | null> {
 		/* The server actually replies to the POST with a 302 (Found) redirect to the resource of the created user.
 		 * The HttpClient or browser itself handles that redirect for us.
-	     */
-		return this.http.post<User>(this.userUrl, userDetails)
+		 */
+		return this.http.post<User>(UserService.apiUsersPath, userDetails)
 			.pipe(
 				catchError(this.handleError<User | null>(`add username=${userDetails.username}`, null))
 			);
 	}
 
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
+	/**
+	 * Handle Http operation that failed.
+	 * Let the app continue.
+	 * @param operation - name of the operation that failed
+	 * @param result - optional value to return as the observable result
+	 */
 	private handleError<T>(operation = 'operation', result?: T) {
 		return (error: any): Observable<T> => {
 
