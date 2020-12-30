c6988e6d152fc45aa9e572fa021cbbf8a3e8369b
Benedict Adamson <badamson@spamcop.net>
2020-12-30 14:03:47 +0000

    Extract FE AbstractKeyValueService

---
+++ MC-front-end/src/main/web/mc-front-end/src/app/service/abstract.key-value.service.ts
@@ -0,0 +1,11 @@
+import { Observable } from 'rxjs';
+
+export abstract class AbstractKeyValueService<KEY, VALUE, SPECIFICATION> {
+
+	abstract getAll(): Observable<VALUE[]> | undefined;
+
+	abstract get(id: KEY): Observable<VALUE | null>;
+
+	abstract add(specification: SPECIFICATION): Observable<VALUE | null> | undefined;
+
+}
