0e49b4eb11ddafc236da19d345153a3f549b902b
Benedict Adamson <badamson@spamcop.net>
2021-01-07 09:33:53 +0000

    Rearrange FE code to clarify relationships.

--- MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/app.module.ts
@@ -11,21 +11,23 @@ import { WINDOW_PROVIDER } from './window.provider';
 import { AbstractGamePlayersService } from './service/abstract.game-players.service';
 import { AbstractSelfService } from './service/abstract.self.service';
 import { AbstractUserBackEndService } from './service/abstract.user.back-end.service';
+import { GamePlayersService } from './service/game-players.service';
+import { HttpUserBackEndService } from './service/http.user.back-end.service';
+import { SelfService } from './service/self.service';
+
 import { AddUserComponent } from './add-user/add-user.component';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { GamesComponent } from './games/games.component';
 import { GamePlayersComponent } from './game-players/game-players.component';
-import { GamePlayersService } from './service/game-players.service';
 import { LoginComponent } from './login/login.component';
 import { ScenarioComponent } from './scenario/scenario.component';
 import { ScenariosComponent } from './scenarios/scenarios.component';
 import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
-import { HttpUserBackEndService } from './service/http.user.back-end.service';
 import { SelfComponent } from './self/self.component';
-import { SelfService } from './service/self.service';
+
 
 @NgModule({
 	imports: [
