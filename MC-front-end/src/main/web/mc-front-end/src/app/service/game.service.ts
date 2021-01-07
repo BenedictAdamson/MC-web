27f8da5f286340cfbfc3364bdc396b154fabe926
Benedict Adamson <badamson@spamcop.net>
2021-01-07 10:41:13 +0000

    Tidy FE GamesService
    Now have GamesOfScenarioService

--- MC-front-end/src/main/web/mc-front-end/src/app/service/game.service.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/service/game.service.ts
@@ -1,5 +1,5 @@
-import { Observable, ReplaySubject, of } from 'rxjs';
-import { catchError, distinctUntilChanged } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -24,9 +24,9 @@ export class GameService {
 		return GameService.getApiGamesPath(id.scenario) + id.created;
 	}
 
-    /**
-     * Get the game that has a given ID.
-     */
+	/**
+	 * Get the game that has a given ID.
+	 */
 	getGame(id: GameIdentifier): Observable<Game | null> {
 		return this.http.get<Game>(GameService.getApiGamePath(id))
 			.pipe(
@@ -34,30 +34,30 @@ export class GameService {
 			);
 	}
 
-    /**
-     * Create a new game for a given scenario.
-     *
-     * @param scenario
-     * The unique ID of the scenario for which to create a gave.
-     * @returns
-     * An [[Observable]] that provides the created game.
-     * The [[GameIdentifier.scenario]] of the [[Game.identifier]] of the created game
-     * is equal to the given {@code scenario}.
-     */
+	/**
+	 * Create a new game for a given scenario.
+	 *
+	 * @param scenario
+	 * The unique ID of the scenario for which to create a gave.
+	 * @returns
+	 * An [[Observable]] that provides the created game.
+	 * The [[GameIdentifier.scenario]] of the [[Game.identifier]] of the created game
+	 * is equal to the given {@code scenario}.
+	 */
 	createGame(scenario: string): Observable<Game> {
 		/* The server actually replies to the POST with a 302 (Found) redirect to the resource of the created game.
 		 * The HttpClient or browser itself handles that redirect for us.
-	     */
+		 */
 		return this.http.post<Game>(GameService.getApiGamesPath(scenario), "");
 	}
 
 
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
+	/**
+	 * Handle Http operation that failed.
+	 * Let the app continue.
+	 * @param operation - name of the operation that failed
+	 * @param result - optional value to return as the observable result
+	 */
 	private handleError<T>(operation = 'operation', result?: T) {
 		return (error: any): Observable<T> => {
 
