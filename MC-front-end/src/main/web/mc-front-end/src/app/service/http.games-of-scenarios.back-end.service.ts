b04fb27cec7b897e9604f370b6d9c2155b060e49
Benedict Adamson <badamson@spamcop.net>
2021-01-13 11:51:32 +0000

    Add FE HttpGamesOfScenarioBackEndService
    and its ABC.

---
+++ MC-front-end/src/main/web/mc-front-end/src/app/service/http.games-of-scenarios.back-end.service.ts
@@ -0,0 +1,72 @@
+import { Observable } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { AbstractGamesOfScenarioBackEndService } from './abstract.games-of-scenarios.back-end.service';
+import { HttpKeyValueService } from './http.key-value.service';
+
+
+
+export function getApiGamesPath(scenario: string): string {
+	return '/api/scenario/' + scenario + '/game/';
+}
+
+
+class Delegate extends HttpKeyValueService<string, string[], void, void> {
+
+	constructor(
+		http: HttpClient
+	) {
+		super(http, undefined);
+	}
+
+
+	getUrl(id: string): string {
+		return getApiGamesPath(id);
+	}
+
+	getAll(): undefined {
+		return undefined;
+	}
+
+	add(_specification: void): undefined {
+		return undefined;
+	}
+
+
+	protected getAddUrl(_specification: void): undefined {
+		return undefined;
+	}
+
+	protected getAddPayload(_specification: void): undefined {
+		return undefined;
+	}
+
+
+}// class
+
+
+
+@Injectable({
+	providedIn: 'root'
+})
+export class HttpGamesOfScenarioBackEndService extends AbstractGamesOfScenarioBackEndService {
+
+	private delegate: Delegate;
+
+
+	constructor(
+		http: HttpClient
+	) {
+		super();
+		this.delegate = new Delegate(http)
+	}
+
+
+	get(id: string): Observable<string[] | null> {
+		return this.delegate.get(id);
+	}
+
+}// class
+
