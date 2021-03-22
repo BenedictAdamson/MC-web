46e7366d088f6450243c26e7884b07460e0b607d
Benedict Adamson <badamson@spamcop.net>
2021-03-22 15:03:23 +0000

    Satisfy ESLint pedantry for FE GamesComponent tests

--- MC-front-end/src/main/web/mc-front-end/src/app/games/games.component.spec.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/games/games.component.spec.ts
@@ -20,216 +20,216 @@ import { MockGamesOfScenarioBackEndService } from '../service/mock/mock.games-of
 
 
 describe('GamesComponent', () => {
-	let routerSpy: any;
-	let gameServiceSpy: any;
-	let gamesOfScenarioBackEndService: MockGamesOfScenarioBackEndService;
-	let selfService: AbstractSelfService;
-	let component: GamesComponent;
-	let fixture: ComponentFixture<GamesComponent>;
+   let routerSpy: any;
+   let gameServiceSpy: any;
+   let gamesOfScenarioBackEndService: MockGamesOfScenarioBackEndService;
+   let selfService: AbstractSelfService;
+   let component: GamesComponent;
+   let fixture: ComponentFixture<GamesComponent>;
 
-	const USER_ADMIN: User = { id: uuid(), username: 'Allan', password: null, authorities: ['ROLE_MANAGE_GAMES'] };
-	const USER_NORMAL: User = { id: uuid(), username: 'Benedict', password: null, authorities: [] };
+   const USER_ADMIN: User = { id: uuid(), username: 'Allan', password: null, authorities: ['ROLE_MANAGE_GAMES'] };
+   const USER_NORMAL: User = { id: uuid(), username: 'Benedict', password: null, authorities: [] };
 
-	const SCENARIO_A: string = uuid();
-	const SCENARIO_B: string = uuid();
-	const CREATED_A: string = '1970-01-01T00:00:00.000Z';
-	const CREATED_B: string = '2020-12-31T23:59:59.999Z';
-	const GAMES_0: string[] = [];
-	const GAMES_2: string[] = [CREATED_A, CREATED_B];
-	const GAME_IDENTIFIER_A: GameIdentifier = { scenario: SCENARIO_A, created: CREATED_A };
-	const GAME_IDENTIFIER_B: GameIdentifier = { scenario: SCENARIO_B, created: CREATED_B };
-	const GAME_A: Game = { identifier: GAME_IDENTIFIER_A };
-	const GAME_B: Game = { identifier: GAME_IDENTIFIER_B };
+   const SCENARIO_A: string = uuid();
+   const SCENARIO_B: string = uuid();
+   const CREATED_A = '1970-01-01T00:00:00.000Z';
+   const CREATED_B = '2020-12-31T23:59:59.999Z';
+   const GAMES_0: string[] = [];
+   const GAMES_2: string[] = [CREATED_A, CREATED_B];
+   const GAME_IDENTIFIER_A: GameIdentifier = { scenario: SCENARIO_A, created: CREATED_A };
+   const GAME_IDENTIFIER_B: GameIdentifier = { scenario: SCENARIO_B, created: CREATED_B };
+   const GAME_A: Game = { identifier: GAME_IDENTIFIER_A };
+   const GAME_B: Game = { identifier: GAME_IDENTIFIER_B };
 
-	const getScenario = function(component: GamesComponent): string | null {
-		var scenario: string | null = null;
-		component.scenario$.subscribe({
-			next: (s) => scenario = s,
-			error: (err) => fail(err),
-			complete: () => { }
-		});
-		return scenario;
-	};
+   const getScenario = (gc: GamesComponent): string | null => {
+      let scenario: string | null = null;
+      gc.scenario$.subscribe({
+         next: (s) => scenario = s,
+         error: (err) => fail(err),
+         complete: () => { }
+      });
+      return scenario;
+   };
 
-	const getGames = function(component: GamesComponent): string[] | null {
-		var games: string[] | null = null;
-		component.games$.subscribe({
-			next: (g) => games = g,
-			error: (err) => fail(err),
-			complete: () => { }
-		});
-		return games;
-	};
+   const getGames = (gc: GamesComponent): string[] | null => {
+      let games: string[] | null = null;
+      gc.games$.subscribe({
+         next: (g) => games = g,
+         error: (err) => fail(err),
+         complete: () => { }
+      });
+      return games;
+   };
 
-	const setUpForNgInit = function(self: User, scenario: string, gamesOfScenario: string[]) {
-		gameServiceSpy = null;
-		gamesOfScenarioBackEndService = new MockGamesOfScenarioBackEndService(scenario, gamesOfScenario);
+   const setUpForNgInit = (self: User, scenario: string, gamesOfScenario: string[]) => {
+      gameServiceSpy = null;
+      gamesOfScenarioBackEndService = new MockGamesOfScenarioBackEndService(scenario, gamesOfScenario);
 
-		TestBed.configureTestingModule({
-			imports: [RouterTestingModule],
-			declarations: [GamesComponent],
-			providers: [{
-				provide: ActivatedRoute,
-				useValue: {
-					parent: {
-						paramMap: of(convertToParamMap({ scenario: scenario })),
-					},
-					snapshot: {
-						parent: {
-							paramMap: convertToParamMap({ scenario: scenario })
-						}
-					}
-				}
-			},
-			{ provide: GameService, useValue: gameServiceSpy },
-			{ provide: AbstractGamesOfScenarioBackEndService, useValue: gamesOfScenarioBackEndService },
-			{ provide: AbstractSelfService, useFactory: () => { return new MockSelfService(self); } }]
-		});
+      TestBed.configureTestingModule({
+         imports: [RouterTestingModule],
+         declarations: [GamesComponent],
+         providers: [{
+            provide: ActivatedRoute,
+            useValue: {
+               parent: {
+                  paramMap: of(convertToParamMap({ scenario })),
+               },
+               snapshot: {
+                  parent: {
+                     paramMap: convertToParamMap({ scenario })
+                  }
+               }
+            }
+         },
+         { provide: GameService, useValue: gameServiceSpy },
+         { provide: AbstractGamesOfScenarioBackEndService, useValue: gamesOfScenarioBackEndService },
+         { provide: AbstractSelfService, useFactory: () => new MockSelfService(self) }]
+      });
 
-		fixture = TestBed.createComponent(GamesComponent);
-		component = fixture.componentInstance;
-		selfService = TestBed.inject(AbstractSelfService);
-		selfService.checkForCurrentAuthentication().subscribe();
-		fixture.detectChanges();
-	};
-
-
-	const assertInvariants = function() {
-		expect(component).toBeTruthy();
-
-		expect(component.scenario$).withContext('scenario$').toBeTruthy();
-		expect(component.games$).withContext('games$').toBeTruthy();
-
-		const html: HTMLElement = fixture.nativeElement;
-		const gamesList: HTMLUListElement | null = html.querySelector('#games');
-		const createGameButton: HTMLButtonElement | null = html.querySelector('button#create-game');
-
-		const createGameButtonText: string = createGameButton ? createGameButton.innerText : "";
-
-		expect(createGameButton).withContext('#create-game button').not.toBeNull();
-		expect(createGameButtonText).withContext('#create-game button text').toEqual('Create game');
-
-		expect(gamesList).withContext('games list').not.toBeNull();
-		if (gamesList) {
-			const gameEntries: NodeListOf<HTMLLIElement> = gamesList.querySelectorAll('li');
-			for (let i = 0; i < gameEntries.length; i++) {
-				const entry: HTMLLIElement = gameEntries.item(i);
-				const link: HTMLAnchorElement | null = entry.querySelector('a');
-				expect(link).withContext('entry has link').not.toBeNull();
-			}
-		}
-	};
+      fixture = TestBed.createComponent(GamesComponent);
+      component = fixture.componentInstance;
+      selfService = TestBed.inject(AbstractSelfService);
+      selfService.checkForCurrentAuthentication().subscribe();
+      fixture.detectChanges();
+   };
 
 
-	const testNgInit = function(self: User, scenario: string, gamesOfScenario: string[]) {
-		setUpForNgInit(self, scenario, gamesOfScenario);
+   const assertInvariants = () => {
+      expect(component).toBeTruthy();
 
-		assertInvariants();
-		expect(getScenario(component)).withContext('scenario$').toEqual(scenario);
-		expect(getGames(component)).withContext('games$').toEqual(gamesOfScenario);
+      expect(component.scenario$).withContext('scenario$').toBeTruthy();
+      expect(component.games$).withContext('games$').toBeTruthy();
 
-		const html: HTMLElement = fixture.nativeElement;
-		const gamesList: HTMLUListElement | null = html.querySelector('#games');
+      const html: HTMLElement = fixture.nativeElement;
+      const gamesList: HTMLUListElement | null = html.querySelector('#games');
+      const createGameButton: HTMLButtonElement | null = html.querySelector('button#create-game');
 
-		if (gamesList) {
-			const gameEntries: NodeListOf<HTMLLIElement> = gamesList.querySelectorAll('li');
-			expect(gameEntries.length).withContext('number of game entries').toBe(gamesOfScenario.length);
-			for (let i = 0; i < gameEntries.length; i++) {
-				const expectedGame: string = gamesOfScenario[i];
-				const entry: HTMLLIElement = gameEntries.item(i);
-				const link: HTMLAnchorElement | null = entry.querySelector('a');
-				const linkText: string | null = link ? link.textContent : null;
-				expect(linkText).withContext('entry link text contains game title').toContain(expectedGame);
-			}
-		}
-	};
-	it('can initialize [a]', () => {
-		testNgInit(USER_ADMIN, SCENARIO_A, GAMES_0);
-	});
-	it('can initialize [b]', () => {
-		testNgInit(USER_NORMAL, SCENARIO_B, GAMES_2);
-	});
+      const createGameButtonText: string = createGameButton ? createGameButton.innerText : '';
+
+      expect(createGameButton).withContext('#create-game button').not.toBeNull();
+      expect(createGameButtonText).withContext('#create-game button text').toEqual('Create game');
+
+      expect(gamesList).withContext('games list').not.toBeNull();
+      if (gamesList) {
+         const gameEntries: NodeListOf<HTMLLIElement> = gamesList.querySelectorAll('li');
+         for (let i = 0; i < gameEntries.length; i++) {
+            const entry: HTMLLIElement = gameEntries.item(i);
+            const link: HTMLAnchorElement | null = entry.querySelector('a');
+            expect(link).withContext('entry has link').not.toBeNull();
+         }
+      }
+   };
 
 
-	const setUpForCreateGame = function(game: Game) {
-		const scenario: string = game.identifier.scenario;
-		const userServiceStub = new MockGamesOfScenarioBackEndService(scenario, []);
+   const testNgInit = (self: User, scenario: string, gamesOfScenario: string[]) => {
+      setUpForNgInit(self, scenario, gamesOfScenario);
 
-		gameServiceSpy = jasmine.createSpyObj('GameService', ['createGame']);
-		gameServiceSpy.createGame.and.returnValue(of(game));
+      assertInvariants();
+      expect(getScenario(component)).withContext('scenario$').toEqual(scenario);
+      expect(getGames(component)).withContext('games$').toEqual(gamesOfScenario);
 
-		routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
-		routerSpy.navigateByUrl.and.returnValue(null);
+      const html: HTMLElement = fixture.nativeElement;
+      const gamesList: HTMLUListElement | null = html.querySelector('#games');
 
-		TestBed.configureTestingModule({
-			declarations: [GamesComponent],
-			providers: [{
-				provide: ActivatedRoute,
-				useValue: {
-					parent: {
-						paramMap: of(convertToParamMap({ scenario: scenario })),
-					},
-					snapshot: {
-						parent: {
-							paramMap: convertToParamMap({ scenario: scenario })
-						}
-					}
-				}
-			},
-			{ provide: AbstractGamesOfScenarioBackEndService, useValue: userServiceStub },
-			{ provide: GameService, useValue: gameServiceSpy },
-			{ provide: Router, useValue: routerSpy },
-			{ provide: AbstractSelfService, useFactory: () => { return new MockSelfService(USER_ADMIN); } }]
-		});
+      if (gamesList) {
+         const gameEntries: NodeListOf<HTMLLIElement> = gamesList.querySelectorAll('li');
+         expect(gameEntries.length).withContext('number of game entries').toBe(gamesOfScenario.length);
+         for (let i = 0; i < gameEntries.length; i++) {
+            const expectedGame: string = gamesOfScenario[i];
+            const entry: HTMLLIElement = gameEntries.item(i);
+            const link: HTMLAnchorElement | null = entry.querySelector('a');
+            const linkText: string | null = link ? link.textContent : null;
+            expect(linkText).withContext('entry link text contains game title').toContain(expectedGame);
+         }
+      }
+   };
+   it('can initialize [a]', () => {
+      testNgInit(USER_ADMIN, SCENARIO_A, GAMES_0);
+   });
+   it('can initialize [b]', () => {
+      testNgInit(USER_NORMAL, SCENARIO_B, GAMES_2);
+   });
 
-		fixture = TestBed.createComponent(GamesComponent);
-		component = fixture.componentInstance;
-		selfService = TestBed.inject(AbstractSelfService);
-		selfService.checkForCurrentAuthentication().subscribe();
-		fixture.detectChanges();
-	};
 
-	const testCreateGame = function(game: Game) {
-		const expectedPath: string = GamesComponent.getGamePagePath(game.identifier);
-		setUpForCreateGame(game);
+   const setUpForCreateGame = (game: Game) => {
+      const scenario: string = game.identifier.scenario;
+      const userServiceStub = new MockGamesOfScenarioBackEndService(scenario, []);
 
-		component.createGame();
-		tick();
-		tick();
-		fixture.detectChanges();
+      gameServiceSpy = jasmine.createSpyObj('GameService', ['createGame']);
+      gameServiceSpy.createGame.and.returnValue(of(game));
 
-		assertInvariants();
-		expect(routerSpy.navigateByUrl.calls.count()).withContext('router.navigateByUrl calls').toEqual(1);
-		expect(routerSpy.navigateByUrl.calls.argsFor(0)).withContext('router.navigateByUrl args').toEqual([expectedPath]);
-	};
+      routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+      routerSpy.navigateByUrl.and.returnValue(null);
 
-	it('can create game [A]', fakeAsync(() => {
-		testCreateGame(GAME_A);
-	}));
+      TestBed.configureTestingModule({
+         declarations: [GamesComponent],
+         providers: [{
+            provide: ActivatedRoute,
+            useValue: {
+               parent: {
+                  paramMap: of(convertToParamMap({ scenario })),
+               },
+               snapshot: {
+                  parent: {
+                     paramMap: convertToParamMap({ scenario })
+                  }
+               }
+            }
+         },
+         { provide: AbstractGamesOfScenarioBackEndService, useValue: userServiceStub },
+         { provide: GameService, useValue: gameServiceSpy },
+         { provide: Router, useValue: routerSpy },
+         { provide: AbstractSelfService, useFactory: () => new MockSelfService(USER_ADMIN) }]
+      });
 
-	it('can create game [B]', fakeAsync(() => {
-		testCreateGame(GAME_B);
-	}));
+      fixture = TestBed.createComponent(GamesComponent);
+      component = fixture.componentInstance;
+      selfService = TestBed.inject(AbstractSelfService);
+      selfService.checkForCurrentAuthentication().subscribe();
+      fixture.detectChanges();
+   };
 
-	it('disables create game button for normal users', () => {
-		setUpForNgInit(USER_NORMAL, SCENARIO_A, []);
+   const testCreateGame = (game: Game) => {
+      const expectedPath: string = GamesComponent.getGamePagePath(game.identifier);
+      setUpForCreateGame(game);
 
-		assertInvariants();
-		const html: HTMLElement = fixture.nativeElement;
-		const createGameButton: HTMLButtonElement | null = html.querySelector('button#create-game');
-		if (createGameButton) {
-			expect(createGameButton.disabled).withContext('create game button is disabled').toBeTrue();
-		}
-	});
+      component.createGame();
+      tick();
+      tick();
+      fixture.detectChanges();
 
-	it('enables create game button for an administrator', () => {
-		setUpForNgInit(USER_ADMIN, SCENARIO_A, []);
+      assertInvariants();
+      expect(routerSpy.navigateByUrl.calls.count()).withContext('router.navigateByUrl calls').toEqual(1);
+      expect(routerSpy.navigateByUrl.calls.argsFor(0)).withContext('router.navigateByUrl args').toEqual([expectedPath]);
+   };
 
-		assertInvariants();
-		const html: HTMLElement = fixture.nativeElement;
-		const createGameButton: HTMLButtonElement | null = html.querySelector('button#create-game');
-		if (createGameButton) {
-			expect(createGameButton.disabled).withContext('create game button is disabled').toBeFalse();
-		}
-	});
+   it('can create game [A]', fakeAsync(() => {
+      testCreateGame(GAME_A);
+   }));
+
+   it('can create game [B]', fakeAsync(() => {
+      testCreateGame(GAME_B);
+   }));
+
+   it('disables create game button for normal users', () => {
+      setUpForNgInit(USER_NORMAL, SCENARIO_A, []);
+
+      assertInvariants();
+      const html: HTMLElement = fixture.nativeElement;
+      const createGameButton: HTMLButtonElement | null = html.querySelector('button#create-game');
+      if (createGameButton) {
+         expect(createGameButton.disabled).withContext('create game button is disabled').toBeTrue();
+      }
+   });
+
+   it('enables create game button for an administrator', () => {
+      setUpForNgInit(USER_ADMIN, SCENARIO_A, []);
+
+      assertInvariants();
+      const html: HTMLElement = fixture.nativeElement;
+      const createGameButton: HTMLButtonElement | null = html.querySelector('button#create-game');
+      if (createGameButton) {
+         expect(createGameButton.disabled).withContext('create game button is disabled').toBeFalse();
+      }
+   });
 });
