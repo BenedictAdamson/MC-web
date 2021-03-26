705319c00a102b45a0c2ae8f9c0a6997144b7720
Benedict Adamson <badamson@spamcop.net>
2021-03-26 10:39:17 +0000

    Refactor FE GamePlayers and GamePlayersComponednt
    To introduce lower level method that can be reused.

--- MC-front-end/src/main/web/mc-front-end/src/app/game-players.ts
+++ MC-front-end/src/main/web/mc-front-end/src/app/game-players.ts
@@ -11,29 +11,33 @@ import { GameIdentifier } from './game-identifier';
  * </p>
  */
 export class GamePlayers {
-	constructor(
-		public game: GameIdentifier,
-		public recruiting: boolean,
-		/**
-		 * <p>
-		 * The (unique IDs of the users
-		 * who played, or are playing, the game, and the IDs
-		 * of the characters they played.
-		 * </p>
-		 * <ul>
-		 * <li>The map maps a character ID to the ID of the user who is playing (or
-		 * played, or will play) that character.</li>
-		 * </ul>
-		 */
-		public users: Map<string, string>
-	) { }
+   constructor(
+      public game: GameIdentifier,
+      public recruiting: boolean,
+      /**
+       * <p>
+       * The (unique IDs of the users
+       * who played, or are playing, the game, and the IDs
+       * of the characters they played.
+       * </p>
+       * <ul>
+       * <li>The map maps a character ID to the ID of the user who is playing (or
+       * played, or will play) that character.</li>
+       * </ul>
+       */
+      public users: Map<string, string>
+   ) { }
 
-	isPlaying(user: string): boolean {
-		for (const u of this.users.values()) {
-			if (user === u) {
-				return true;
-			}
-		}
-		return false;
-	}
+   characterOfUser(user: string): string | null {
+      for (const e of this.users.entries()) {
+         if (user === e[1]) {
+            return e[0];
+         }
+      }
+      return null;
+   }
+
+   isPlaying(user: string): boolean {
+      return this.characterOfUser(user) != null;
+   }
 }
