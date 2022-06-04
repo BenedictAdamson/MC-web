a675af90476b6f212844b31a5f96d426e8fd5ff7
Benedict Adamson <badamson@spamcop.net>
2022-06-04 11:51:44 +0100

    Convert all Spock ancillary code (back) to Java

---
+++ MC-integration/src/test/java/uk/badamson/mc/MockMcBackEndContainer.java
@@ -0,0 +1,360 @@
+package uk.badamson.mc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.matchers.Times;
+import org.mockserver.model.*;
+import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public final class MockMcBackEndContainer extends MockServerContainer {
+    private static String gamePath(final Game.Identifier game) {
+        Objects.requireNonNull(game, "game");
+        return gamesListPath(game.getScenario()) + URI_DATETIME_FORMATTER.format(game.getCreated());
+    }
+
+    private static String gamePlayersPath(final Game.Identifier game) {
+        return gamePath(game) + "/players";
+    }
+
+    private static String gamesListPath(final UUID scenario) {
+        return scenarioPath(scenario) + "/game/";
+    }
+
+    private static String scenarioPath(final UUID scenario) {
+        Objects.requireNonNull(scenario, "scenario");
+        return "/api/scenario/" + scenario;
+    }
+
+    private static String userPath(@Nonnull final UUID id) {
+        Objects.requireNonNull(id, "id");
+        return "/api/user/" + id;
+    }
+
+    private static String encodeAsJson(final Object obj) {
+        try {
+            return MAPPER.writeValueAsString(obj);
+        } catch (final JsonProcessingException e) {
+            throw new IllegalArgumentException("can not encode Object as JSON", e);
+        }
+
+    }
+
+    public MockMcBackEndContainer() {
+        super(MOCKSERVER_IMAGE);
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        mockServerClient = new MockServerClient(getHost(), getServerPort());
+    }
+
+    @Override
+    public void stop() {
+        if (mockServerClient != null) {
+            mockServerClient.stop();
+            mockServerClient = null;
+        }
+
+        super.stop();
+    }
+
+    @Override
+    public void close() {
+        super.close();
+    }
+
+    public void reset() {
+        if (mockServerClient != null) {
+            mockServerClient.reset();
+        }
+
+    }
+
+    public void mockCreateGameForScenario(@Nonnull final Game.Identifier gameId) {
+        mockServerClient.when(createGameForScenarioRequest(gameId.getScenario())).respond(createGameForScenarioResponse(gameId));
+    }
+
+    private static HttpRequest createGameForScenarioRequest(@Nonnull UUID scenario) {
+        return HttpRequest.request().withMethod("POST").withPath(gamesListPath(scenario));
+    }
+
+    private static HttpResponse createGameForScenarioResponse(@Nonnull final Game.Identifier gameId) {
+        return foundResponse(gamePath(gameId));
+    }
+
+    private static HttpResponse foundResponse(@Nonnull String path) {
+        return HttpResponse.response().withStatusCode(HttpStatusCode.FOUND_302.code()).withHeader(Header.header("Location", path));
+    }
+
+    private static HttpResponse unauthorisedResponse() {
+        return HttpResponse.response().withStatusCode(HttpStatusCode.UNAUTHORIZED_401.code());
+    }
+
+    public void mockGetGameCreationTimes(@Nonnull UUID scenario, @Nonnull Set<Instant> gameCreationTimes, Times times) {
+        mockServerClient.when(getGameCreationTimesRequest(scenario), times).respond(getGameCreationTimesResponse(gameCreationTimes));
+    }
+
+    public void mockGetGameCreationTimes(@Nonnull UUID scenario, @Nonnull Set<Instant> gameCreationTimes) {
+        mockGetGameCreationTimes(scenario, gameCreationTimes, Times.unlimited());
+    }
+
+    private static HttpRequest getGameCreationTimesRequest(@Nonnull UUID scenario) {
+        return HttpRequest.request(gamesListPath(scenario)).withMethod("GET");
+    }
+
+    private static HttpResponse getGameCreationTimesResponse(@Nonnull Set<Instant> times) {
+        List<String> formattedTimes = times.stream().map(URI_DATETIME_FORMATTER::format).collect(Collectors.toUnmodifiableList());
+        return jsonResponse(formattedTimes);
+    }
+
+    private static HttpResponse jsonResponse(@Nullable Object body) {
+        return HttpResponse.response().withContentType(MediaType.APPLICATION_JSON).withBody(encodeAsJson(body));
+    }
+
+    public void mockGetGame(@Nonnull final Game game, Times times) {
+        mockServerClient.when(getGameRequest(game.getIdentifier()), times).respond(getGameResponse(game));
+    }
+
+    public void mockGetGame(@Nonnull final Game game) {
+        mockGetGame(game, Times.unlimited());
+    }
+
+    private static HttpRequest getGameRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePath(game)).withMethod("GET");
+    }
+
+    private static HttpResponse getGameResponse(@Nonnull final Game game) {
+        return jsonResponse(game);
+    }
+
+    public void mockStartGame(@Nonnull final Game.Identifier game) {
+        mockServerClient.when(startGameRequest(game)).respond(startGameResponse(game));
+    }
+
+    private static HttpRequest startGameRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePath(game)).withQueryStringParameter("start", "").withMethod("POST");
+    }
+
+    private static HttpResponse startGameResponse(@Nonnull final Game.Identifier game) {
+        return foundResponse(gamePath(game));
+    }
+
+    public void mockStopGame(@Nonnull final Game.Identifier game) {
+        mockServerClient.when(stopGameRequest(game)).respond(stopGameResponse(game));
+    }
+
+    private static HttpRequest stopGameRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePath(game)).withQueryStringParameter("stop", "").withMethod("POST");
+    }
+
+    private static HttpResponse stopGameResponse(@Nonnull final Game.Identifier game) {
+        return foundResponse(gamePath(game));
+    }
+
+    public void mockEndRecruitment(@Nonnull final Game.Identifier game) {
+        mockServerClient.when(endRecruitmentRequest(game)).respond(endRecruitmentResponse(game));
+    }
+
+    private static HttpRequest endRecruitmentRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePlayersPath(game)).withQueryStringParameter("endRecruitment", "").withMethod("POST");
+    }
+
+    private static HttpResponse endRecruitmentResponse(@Nonnull final Game.Identifier game) {
+        return foundResponse(gamePlayersPath(game));
+    }
+
+    public void mockCurrentGame(@Nonnull final Game.Identifier game) {
+        Objects.requireNonNull(game, "game");
+        mockServerClient.when(currentGameRequest()).respond(currentGameResponse(game));
+    }
+
+    public void mockNoCurrentGame() {
+        mockServerClient.when(currentGameRequest()).respond(HttpResponse.notFoundResponse());
+    }
+
+    private static HttpRequest currentGameRequest() {
+        return HttpRequest.request("/api/self/current-game").withMethod("GET");
+    }
+
+    private static HttpResponse currentGameResponse(@Nonnull final Game.Identifier game) {
+        return foundResponse(gamePath(game));
+    }
+
+    public void mockGetGamePlayers(@Nonnull GamePlayers players, Times times) {
+        mockServerClient.when(getGamePlayersRequest(players.getGame()), times).respond(getGamePlayersResponse(players));
+    }
+
+    public void mockGetGamePlayers(@Nonnull GamePlayers players) {
+        mockGetGamePlayers(players, Times.unlimited());
+    }
+
+    private static HttpRequest getGamePlayersRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePlayersPath(game)).withMethod("GET").withQueryStringParameter("!mayJoin");
+    }
+
+    private static HttpResponse getGamePlayersResponse(@Nonnull GamePlayers players) {
+        return jsonResponse(players);
+    }
+
+    public void mockJoinGame(@Nonnull final Game.Identifier game) {
+        mockServerClient.when(joinGameRequest(game)).respond(joinGameResponse(game));
+    }
+
+    private static HttpRequest joinGameRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePlayersPath(game)).withQueryStringParameter("join", "").withMethod("POST");
+    }
+
+    private static HttpResponse joinGameResponse(@Nonnull final Game.Identifier game) {
+        return foundResponse(gamePlayersPath(game));
+    }
+
+    public void mockMayJoinGame(@Nonnull final Game.Identifier game, final boolean mayJoin) {
+        mockServerClient.when(mayJoinGameRequest(game)).respond(mayJoinGameResponse(mayJoin));
+    }
+
+    private static HttpRequest mayJoinGameRequest(@Nonnull final Game.Identifier game) {
+        return HttpRequest.request(gamePlayersPath(game)).withQueryStringParameter("mayJoin", "").withMethod("GET");
+    }
+
+    private static HttpResponse mayJoinGameResponse(final boolean mayJoin) {
+        return jsonResponse(mayJoin);
+    }
+
+    public void mockGetAllScenarios(@Nonnull final Set<NamedUUID> scenarios) {
+        mockServerClient.when(getAllScenariosRequest()).respond(getAllScenariosResponse(scenarios));
+    }
+
+    private static HttpRequest getAllScenariosRequest() {
+        return HttpRequest.request("/api/scenario").withMethod("GET");
+    }
+
+    private static HttpResponse getAllScenariosResponse(@Nonnull final Set<NamedUUID> scenarios) {
+        return jsonResponse(scenarios);
+    }
+
+    public void mockGetScenario(@Nonnull final Scenario scenario) {
+        mockServerClient.when(getScenarioRequest(scenario.getIdentifier())).respond(getScenarioResponse(scenario));
+    }
+
+    private static HttpRequest getScenarioRequest(@Nonnull UUID scenario) {
+        return HttpRequest.request(scenarioPath(scenario)).withMethod("GET");
+    }
+
+    private static HttpResponse getScenarioResponse(@Nonnull final Scenario scenario) {
+        Objects.requireNonNull(scenario, "scenario");
+        return jsonResponse(scenario);
+    }
+
+    public void mockAddUser(@Nonnull final BasicUserDetails userDetails, @Nonnull final UUID id) {
+        mockServerClient.when(addUserRequest(userDetails)).respond(addUserResponse(id));
+    }
+
+    private static HttpRequest addUserRequest(@Nonnull final BasicUserDetails userDetails) {
+        return HttpRequest.request("/api/user").withMethod("POST").withBody(encodeAsJson(new MinimalUserDetails(userDetails)));
+    }
+
+    private static HttpResponse addUserResponse(@Nonnull final UUID id) {
+        return foundResponse(userPath(id));
+    }
+
+    public void mockGetAllUsers(@Nonnull final Set<User> users, @Nonnull Times times) {
+        mockServerClient.when(getAllUsersRequest(), times).respond(getAllUsersResponse(users));
+    }
+
+    public void mockGetAllUsers(@Nonnull final Set<User> users) {
+        mockGetAllUsers(users, Times.unlimited());
+    }
+
+    private static HttpRequest getAllUsersRequest() {
+        return HttpRequest.request("/api/user").withMethod("GET");
+    }
+
+    private static HttpResponse getAllUsersResponse(@Nonnull final Set<User> users) {
+        return jsonResponse(users);
+    }
+
+    public void mockGetSelf(@Nonnull final User user, Times times) {
+        mockServerClient.when(getSelfRequest(), times).respond(getSelfResponse(user));
+    }
+
+    public void mockGetSelf(@Nonnull final User user) {
+        mockGetSelf(user, Times.unlimited());
+    }
+
+    public void mockGetSelfUnauthenticated(Times times) {
+        mockServerClient.when(getSelfRequest(), times).respond(unauthorisedResponse());
+    }
+
+    public void mockGetSelfUnauthenticated() {
+        mockGetSelfUnauthenticated(Times.unlimited());
+    }
+
+    private static HttpRequest getSelfRequest() {
+        return HttpRequest.request("/api/self").withMethod("GET");
+    }
+
+    private static HttpResponse getSelfResponse(@Nonnull final User user) {
+        return jsonResponse(user);
+    }
+
+    public void mockGetUser(@Nonnull final User user) {
+        mockServerClient.when(getUserRequest(user.getId())).respond(getUserResponse(user));
+    }
+
+    private static HttpRequest getUserRequest(@Nonnull final UUID id) {
+        return HttpRequest.request(userPath(id)).withMethod("GET");
+    }
+
+    private static HttpResponse getUserResponse(@Nonnull final User user) {
+        Objects.requireNonNull(user, "user");
+        return jsonResponse(user);
+    }
+
+    public void mockLogin(@Nonnull User user, @Nonnull final String sessionCookie, @Nonnull String xsrfToken) {
+        mockServerClient.when(loginRequest()).respond(loginResponse(user, sessionCookie, xsrfToken));
+    }
+
+    private static HttpRequest loginRequest() {
+        return HttpRequest.request("/api/self").withMethod("GET").withHeader(Header.header("Authorization", "Basic .*"));
+    }
+
+    private static HttpResponse loginResponse(@Nonnull User user, @Nonnull final String sessionCookie, @Nonnull String xsrfToken) {
+        return HttpResponse.response().withCookie("JSESSIONID", sessionCookie).withCookie("XSRF-TOKEN", xsrfToken).withContentType(MediaType.APPLICATION_JSON).withBody(encodeAsJson(user));
+    }
+
+    private static final DockerImageName MOCKSERVER_IMAGE = DockerImageName.parse("mockserver/mockserver:5.13.2");
+    private static final DateTimeFormatter URI_DATETIME_FORMATTER = DateTimeFormatter.ISO_INSTANT;
+    private static final ObjectMapper MAPPER;
+
+    static {
+        MAPPER = new ObjectMapper();
+        MAPPER.registerModule(new JavaTimeModule());
+    }
+
+    private MockServerClient mockServerClient;
+
+    final private static class MinimalUserDetails {
+        public MinimalUserDetails(@Nonnull final BasicUserDetails userDetails) {
+            this.username = userDetails.getUsername();
+            this.password = userDetails.getPassword();
+        }
+
+        public final String username;
+        public final String password;
+        public final Set<Authority> authorities = Set.of();
+    }
+}
---
+++ MC-integration/src/test/java/uk/badamson/mc/MockedBeWorld.java
@@ -0,0 +1,197 @@
+package uk.badamson.mc;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.lifecycle.Startable;
+import org.testcontainers.lifecycle.TestDescription;
+import org.testcontainers.lifecycle.TestLifecycleAware;
+import org.testcontainers.utility.DockerImageName;
+import uk.badamson.mc.presentation.HomePage;
+import uk.badamson.mc.presentation.McFrontEndContainer;
+import uk.badamson.mc.presentation.McReverseProxyContainer;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public final class MockedBeWorld implements Startable, TestLifecycleAware {
+
+    public MockedBeWorld(@Nullable final Path failureRecordingDirectory) {
+        fe.withNetwork(network).withNetworkAliases(FE_HOST);
+        ms.withNetwork(network).withNetworkAliases(MS_HOST);
+        ingress.withNetwork(network).withNetworkAliases(INGRESS_HOST);
+        this.failureRecordingDirectory = failureRecordingDirectory;
+        browser.withCapabilities(new FirefoxOptions().addPreference("security.insecure_field_warning.contextual.enabled", false)).withNetwork(network);
+        if (failureRecordingDirectory != null) {
+            try {
+                Files.createDirectories(failureRecordingDirectory);
+            } catch (final IOException e) {
+                throw new IllegalArgumentException(e);
+            }
+            browser.withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, failureRecordingDirectory.toFile());
+        }
+
+    }
+
+    public MockedBeWorld() {
+        this(DEFAULT_FAILURE_RECORDING_DIRECTORY);
+    }
+
+    public MockMcBackEndContainer getBackEnd() {
+        return ms;
+    }
+
+    @Override
+    public void close() {
+        cleanup();
+        stop();
+        network.close();
+    }
+
+    public User createUserWithRole(Authority role) {
+        return createUser(EnumSet.of(role));
+    }
+
+    private User createUser(final Set<Authority> authorities) {
+        final BasicUserDetails userDetails = generateBasicUserDetails(authorities);
+        final UUID id = UUID.randomUUID();
+        return new User(id, userDetails);
+    }
+
+    @Override
+    public void start() {
+        ms.start();
+        fe.start();
+        ingress.start();
+        browser.start();
+        webDriver = browser.getWebDriver();
+    }
+
+    private void cleanup() {
+    }
+
+    public void retainScreenshot(@Nonnull final String baseFileName) {
+        if (failureRecordingDirectory != null) {
+            final String leafName = baseFileName + ".png";
+            final Path path = failureRecordingDirectory.resolve(leafName);
+            try {
+                final var bytes = webDriver.getScreenshotAs(OutputType.BYTES);
+                Files.write(path, bytes);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+
+    private void retainLogFiles(@Nonnull final String baseFileName) {
+        retainLogFile(failureRecordingDirectory, baseFileName, MS_HOST, ms);
+        retainLogFile(failureRecordingDirectory, baseFileName, FE_HOST, fe);
+        retainLogFile(failureRecordingDirectory, baseFileName, INGRESS_HOST, ingress);
+    }
+
+    private static void retainLogFile(final Path directory, final String baseFileName, final String host, final GenericContainer<?> container) {
+        final String leafName = baseFileName + "-" + host + ".log";
+        final Path path = directory.resolve(leafName);
+        try {
+            Files.writeString(path, container.getLogs(), StandardCharsets.UTF_8);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void stop() {
+        cleanup();
+        browser.stop();
+        ingress.stop();
+        fe.stop();
+        ms.stop();
+    }
+
+    @Override
+    public void beforeTest(final TestDescription description) {
+        webDriver.manage().deleteAllCookies();
+        browser.beforeTest(description);
+        ms.reset();
+    }
+
+    @Override
+    public void afterTest(final TestDescription description, final Optional<Throwable> throwable) {
+        browser.afterTest(description, throwable);
+        if (failureRecordingDirectory != null) {
+            String baseFileName = description.getFilesystemFriendlyName();
+            retainLogFiles(baseFileName);
+            retainScreenshot(baseFileName);
+        }
+
+        cleanup();
+    }
+
+    private BasicUserDetails generateBasicUserDetails(final Set<Authority> authorities) {
+        final var sequenceId = ++nUsers;
+        final var username = "User " + sequenceId;
+        final var password = "password" + sequenceId;
+        return new BasicUserDetails(username, password, authorities, true, true, true, true);
+    }
+
+    public HomePage getHomePage() {
+        final HomePage homePage = new HomePage(getWebDriver());
+        homePage.get();
+        return homePage;
+    }
+
+    private WebDriver getWebDriver() {
+        Objects.requireNonNull(webDriver, "webDriver");
+        return webDriver;
+    }
+
+    public void notLoggedIn() {
+        currentLoggedInUser = null;
+        getBackEnd().mockGetSelfUnauthenticated();
+    }
+
+    public HomePage logInAsUserWithTheRole(final Authority role) {
+        return logInAsUser(createUser(EnumSet.of(role)));
+    }
+
+    public HomePage logInAsUserWithoutTheRole(final Authority role) {
+        final Set<Authority> roles = EnumSet.complementOf(EnumSet.of(role));
+        return logInAsUser(createUser(roles));
+    }
+
+    public HomePage logInAsUser(final User user) {
+        currentLoggedInUser = user;
+        getBackEnd().mockLogin(user, UUID.randomUUID().toString(), UUID.randomUUID().toString());
+        getBackEnd().mockGetSelf(currentLoggedInUser);
+        HomePage homePage = getHomePage();
+        homePage.awaitIsReady();
+        return homePage;
+    }
+
+    private static final String FE_HOST = "fe";
+    private static final String MS_HOST = "ms";
+    private static final String INGRESS_HOST = "in";
+    private static final Path DEFAULT_FAILURE_RECORDING_DIRECTORY = Path.of(".", "target", "test-logs");
+    private static final DockerImageName BROWSER_IMAGE_NAME = DockerImageName.parse("selenium/standalone-firefox:4.1.4");
+    private final Path failureRecordingDirectory;
+    private final Network network = Network.newNetwork();
+    private final McFrontEndContainer fe = new McFrontEndContainer();
+    private final MockMcBackEndContainer ms = new MockMcBackEndContainer();
+    private final McReverseProxyContainer ingress = McReverseProxyContainer.createWithMockBe();
+    private final BrowserWebDriverContainer<?> browser = new BrowserWebDriverContainer<>(BROWSER_IMAGE_NAME);
+    private RemoteWebDriver webDriver;
+    private int nUsers;
+    private User currentLoggedInUser;
+}
